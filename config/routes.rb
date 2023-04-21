fb78cddd20d42d3113a86f8d0de91f96ba6794b7
Kohei Higashi <112879983+koheiino@users.noreply.github.com>
2023-04-21 11:54:23 +0000

    [Finish]レイアウト完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # resources :books
-  # resources :posts
-  
-  # get 'books/new'
-  # post 'books' => 'books#create' 
-  # get 'books/:id' => 'books#show'
-  resources :books
   root 'books#index'
+  resources :books
 end
